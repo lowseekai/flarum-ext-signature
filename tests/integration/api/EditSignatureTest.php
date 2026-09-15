1b65540555c1c9a007d8bbf656ea0907f55c1172
gungun88 <pengqibao3@gmail.com>
2026-09-16 03:31:55 +0800

    Update signature integration tests for PHPUnit 12

--- tests/integration/api/EditSignatureTest.php
+++ tests/integration/api/EditSignatureTest.php
@@ -5,6 +5,7 @@ namespace Gtdxyz\Signature\Tests\integration\api;
 use Flarum\Testing\integration\RetrievesAuthorizedUsers;
 use Flarum\Testing\integration\TestCase;
 use Flarum\User\User;
+use PHPUnit\Framework\Attributes\Test;
 
 class EditSignatureTest extends TestCase
 {
@@ -38,9 +39,7 @@ class EditSignatureTest extends TestCase
         ]);
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function user_can_edit_own_signature_when_allowed_to_have_one()
     {
         $response = $this->send(
@@ -69,9 +68,7 @@ class EditSignatureTest extends TestCase
         $this->assertEquals('<t>This is my new signature</t>', $user->signature);
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function user_with_edit_permission_cannot_edit_admin_signature()
     {
         $response = $this->send(
